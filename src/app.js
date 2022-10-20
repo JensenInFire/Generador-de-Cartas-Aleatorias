1c518768ad465c13f324ec17434a124b6961ac7c
JensenInFire <alimendezgt@gmail.com>
2022-10-20 18:54:23 +0000

    random card

--- src/app.js
+++ src/app.js
@@ -2,10 +2,21 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = numerorandom();
+};
+
+let numerorandom = () => {
+  let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "10", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * numeros.length);
+
+  return numeros[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suits = ["diamon", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suits.length);
+  return suits[indexSuit];
 };
